01c7a50a5885ed1c1a2d48a8bebbbde898002c75
Dan Rice <dan.rice.92@outlook.com>
2018-12-02 00:30:46 -0700

    Header and individual page pack tags

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -1,10 +1,18 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
 import React from 'react';
-// general application content can go here, possibly the page header and such
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+export default class Application extends React.Component {
+  render() {
+    return(
+      <h1>Novum Opus</h1>
+    );
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Application />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
--- app/javascript/packs/landing.jsx
+++ app/javascript/packs/landing.jsx
@@ -5,10 +5,7 @@ import PropTypes from 'prop-types';
 export default class Landing extends React.Component {
   render() {
     return(
-      <div>
-        <h1>Novum Opus</h1>
-        <p>Novum Opus is awesome you should totally use it.</p>
-      </div>
+      <p>Novum Opus is awesome you should totally use it.</p>
     );
   }
 }
