157ed201161f91151afdd90689e302c761dca164
Dan Rice <dan.rice.92@outlook.com>
2018-12-02 16:30:25 -0700

    better text

--- app/javascript/packs/landing.jsx
+++ app/javascript/packs/landing.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 export default class Landing extends React.Component {
   render() {
     return(
-      <p>Novum Opus is awesome you should totally use it.</p>
+      <p>Novum Opus is a social platform that allows peers to share information about job and career path prospects.</p>
     );
   }
 }
