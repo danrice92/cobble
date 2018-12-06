9f19bc2b3eb7f77742438eb2052a073c976fc619
Dan Rice <dan.rice.92@outlook.com>
2018-12-06 16:46:16 -0700

    Submit Job Experiences

--- app/controllers/job_experiences_controller.rb
+++ app/controllers/job_experiences_controller.rb
@@ -1,5 +1,32 @@
 class JobExperiencesController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def new
     render react_component: "NewJobExperience"
+  end
+
+  def create
+    job_experience = JobExperience.new job_experience_params
+    if job_experience.save
+      puts "job experience saved: #{job_experience_params}"
+      render json: job_experience
+    else
+      render json: { error: job_experience.full_messages.to_sentence }, status: 422
+    end
+  end
+
+  private
+
+  def job_experience_params
+    params.require(:job_experience).permit(
+      :position,
+      :company,
+      :city,
+      :state,
+      :experience,
+      :pay,
+      :recommendation,
+      :website
+    )
   end
 end
--- app/javascript/packs/newJobExperience.jsx
+++ app/javascript/packs/newJobExperience.jsx
@@ -4,20 +4,113 @@ import { Container, Header, Form, Input, Select, TextArea, Button } from "semant
 import Navigation from "./navigation";
 
 class NewJobExperienceContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: "",
+      company: "",
+      city: "",
+      state: "",
+      experience: "",
+      pay: "",
+      recommendation: "",
+      website: ""
+    };
+  }
+
+  updateField = (event) => {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
+  submitForm = (event) => {
+    event.preventDefault();
+
+    fetch("/job_experiences", {
+      method: "POST",
+      headers: { Accept: "application/json", "Content-Type": "application/json" },
+      body: JSON.stringify({ job_experience: { ...this.state } })
+    })
+    .then(function(response) {
+      if(response.ok) { window.location.href = "/" }
+    })
+    .catch(function(error) {
+      console.log('Bad server response', error)
+    })
+  }
+
   render() {
+    const {position, company, city, state, experience, pay, recommendation, website} = this.state;
+
     return(
       <Container>
         <Header size="medium">Create a job experience</Header>
-        <Form>
-          <Form.Field control={Input} id="Position" label="Job Position" placeholder="Cashier" />
-          <Form.Field control={Input} id="Company" label="Company" placeholder="WalMart" />
-          <Form.Field control={Input} id="City" label="City" placeholder="Loveland" />
-          <Form.Field control={Input} id="State" label="State" placeholder="CO" />
-          <Form.Field control={TextArea} id="Experience" label="Experience" placeholder="This job was easy, but the pay was horrible." />
-          <Form.Field control={TextArea} id="Pay" label="Pay" placeholder="I got paid $11/hour. There were no other major perks to the job." />
-          <Form.Field control={TextArea} id="Recommendation" label="Recommendation" placeholder="If you need a job really badly, you could do worse than this. Otherwise, stay away!" />
-          <Form.Field control={Input} id="Website" label="Website" placeholder="www.walmart.com" />
-          <Form.Field control={Button}>Submit</Form.Field>
+        <Form onSubmit={this.submitForm}>
+          <Form.Field
+            control={Input}
+            onChange={this.updateField}
+            id="position"
+            label="Job Position"
+            placeholder="Cashier"
+            value={position}
+          />
+          <Form.Field
+            control={Input}
+            onChange={this.updateField}
+            id="company"
+            label="Company"
+            placeholder="WalMart"
+            value={company}
+          />
+          <Form.Field
+            control={Input}
+            onChange={this.updateField}
+            id="city"
+            label="City"
+            placeholder="Loveland"
+            value={city}
+          />
+          <Form.Field
+            control={Input}
+            onChange={this.updateField}
+            id="state"
+            label="State"
+            placeholder="CO"
+            value={state}
+          />
+          <Form.Field
+            control={TextArea}
+            onChange={this.updateField}
+            id="experience"
+            label="Experience"
+            placeholder="This job was easy, but the pay was horrible."
+            value={experience}
+          />
+          <Form.Field
+            control={TextArea}
+            onChange={this.updateField}
+            id="pay"
+            label="Pay"
+            placeholder="I got paid $11/hour. There were no other major perks to the job."
+            value={pay}
+          />
+          <Form.Field
+            control={TextArea}
+            onChange={this.updateField}
+            id="recommendation"
+            label="Recommendation"
+            placeholder="If you need a job really badly, you could do worse than this. Otherwise, stay away!"
+            value={recommendation}
+          />
+          <Form.Field
+            control={Input}
+            onChange={this.updateField}
+            id="website"
+            label="Website"
+            placeholder="www.walmart.com"
+            value={website}
+          />
+          <Form.Field
+            control={Button}>Submit</Form.Field>
         </Form>
       </Container>
     )
