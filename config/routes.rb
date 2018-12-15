6b1df98a5cc51511840f31d75e857e96633e1965
Dan Rice <dan.rice.92@outlook.com>
2018-12-15 16:43:14 -0700

    Create User model and sign-up form. It creates the User, but the magic link info is currently a lie.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "landings#index"
 
+  resource :users, only: [:new, :create]
+
   resources :job_experiences, only: [:new, :create]
 end
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+feature "sign up" do
+  scenario "a user signs up for a new account" do
+    visit root_path
+    click_on "Sign Up"
+    expect(page).to have_content "Sign Up Form"
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Smith"
+    fill_in "Email", with: "john.smith@example.com"
+    click_on "Create Account"
+    expect(page).to have_content "Thanks for signing up! Your verification email has been sent."
+  end
+end
