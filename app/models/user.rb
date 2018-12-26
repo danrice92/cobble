9a778a4884bdc8bb4d21b62d4a8e518068287044
Dan Rice <dan.rice.92@outlook.com>
2018-12-26 15:37:33 -0700

    Refactors

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,20 +9,12 @@ class User < ApplicationRecord
   end
 
   def send_sign_up_link
-   self.auth_token = generate_token
-   self.auth_token_created_at = Time.now.utc
-   save!
-
-   path = "http://localhost:3000/auth/#{self.id}?token=#{self.auth_token}"
-   UserMailer.with(user: self, path: path).sign_up_email.deliver_now
+    generate_token_and_path self
+    UserMailer.with(user: self, path: path).sign_up_email.deliver_now
   end
 
   def send_sign_in_link
-    self.auth_token = generate_token
-    self.auth_token_created_at = Time.now.utc
-    save!
-
-    path = "http://localhost:3000/auth/#{self.id}?token=#{self.auth_token}"
+    generate_token_and_path self
     UserMailer.with(user: self, path: path).sign_in_email.deliver_now
   end
 
@@ -41,8 +33,12 @@ class User < ApplicationRecord
 
   private
 
-  def generate_token
-   SecureRandom.base64(10)
+  def generate_token_and_path self
+    self.auth_token = SecureRandom.base64(10)
+    self.auth_token_created_at = Time.now.utc
+    save!
+
+    path = "http://localhost:3000/auth/#{self.id}?token=#{self.auth_token}"
   end
 
   def token_validity
