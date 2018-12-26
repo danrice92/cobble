9960b9270e79557c9d05501954dd8dc25300d01f
Dan Rice <dan.rice.92@outlook.com>
2018-12-26 15:30:55 -0700

    update routes, rename view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   root "landings#index"
 
+  # account creation
   resources :users, only: [:create]
-  get "/sign_up", to: "users#sign_up"
-  resources :sessions, only: [:new, :create]
+  get "/sign_up", to: "users#new", as: :sign_up
+
+  # logins
+  resources :sessions, only: [:create]
+  get "/sign_in", to: "sessions#new", as: :sign_in
+
+  # authenticate token
   get "/auth/:id", to: "sessions#auth"
 
   resources :job_experiences, only: [:new, :create]
