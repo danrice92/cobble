b10d943dbc16cf7d90a96cf6351a9898fbd2e16e
Dan Rice <dan.rice.92@outlook.com>
2019-01-01 18:01:05 -0700

    sign_in rather than set_rack_session, policy testing

--- spec/features/edit_job_experience_spec.rb
+++ spec/features/edit_job_experience_spec.rb
@@ -2,26 +2,43 @@ require "rails_helper"
 
 feature "edit job experience" do
   let(:user) { create :user }
+  let(:the_other_guy) { create :user }
   let!(:job_experience) { create :job_experience, creator_id: user.id }
 
   scenario "a user updates a job experience they own" do
-    page.set_rack_session email: user.email
+    sign_in user
     visit root_path
     expect(page).to have_content(job_experience.company)
-
     click_on "Update"
     expect(page).to have_content "Update Your Job Experience"
 
     fill_in "Position", with: "Cashier"
     fill_in "Company", with: "Super Target"
-    fill_in "City", with: "Fort Collins"
-    fill_in "State", with: "CO"
     fill_in "Experience", with: "Cashiering is pretty exhausting. Customers love to complain to cashiers, so you'll be hearing a lot of that. Plus, it doesn't pay much."
     fill_in "Pay", with: "I worked at Super Target in 2010, so the pay went from $8/hour to $8.10/hour while I was there. It's probably slightly better now, but not by much."
-    fill_in "Recommendation", with: "Unless you desperately need a job, I would not recommend working at Super Target."
-    fill_in "Website", with: "www.target.com"
     click_on "Update"
-    expect(page).to have_content("Your experience has been updated.")
+    expect(page).to have_content("This experience has been updated.")
     expect(page).to have_content("Super Target")
+    expect(job_experience.reload.creator).to eq(user)
+  end
+
+  scenario "a user updates a job experience they don't own" do
+    sign_in the_other_guy
+    visit root_path
+    click_on "Update"
+    expect(page).to have_content "Update Your Job Experience"
+
+    fill_in "Pay", with: "Cashiering at Target now pays $12/hour."
+    click_on "Update"
+    expect(page).to have_content("This experience has been updated.")
+    expect(page).to have_content("Cashiering at Target now pays $12/hour.")
+    expect(job_experience.reload.creator).to eq(user)
+  end
+
+  scenario "a user is not signed in and tries to update a job experience" do
+    visit root_path
+    expect(page).to_not have_content "Update"
+    visit edit_job_experience_path job_experience
+    expect(page).to have_content "You are not authorized to perform this action."
   end
 end
