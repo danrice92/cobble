9fa7b176b30d1c579d6b2a401ccf4c6a32bd826a
Dan Rice <dan.rice.92@outlook.com>
2019-07-04 15:11:27 -0600

    well, keep that

--- spec/features/consultation_spec.rb
+++ spec/features/consultation_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 feature "scheduling a consultation" do
   let(:user) { create :user }
 
+  scenario "a user with a verified email wants to schedule a consultation", js: true do
     sign_in user
     visit new_consultation_path
     within_frame find("iframe") do
