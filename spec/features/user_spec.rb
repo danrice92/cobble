c2c4d8a36b1a23341811f1b06003342b336ab846
Dan Rice <dan.rice.92@outlook.com>
2019-07-06 16:23:40 -0600

    Update spec/features/user_spec.rb

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -25,7 +25,7 @@ feature "user sign up and session management" do
     open_email "kingdaphnes@loz.com"
     current_email.click_link "this link"
 
-    expect(page).to have_content "That link has expired. Please go to the sign in page and try again."
+    expect(page).to have_content "That link has expired. Please try signing in again."
   end
 
   scenario "a user tries to sign up with an email that's already in the database" do
